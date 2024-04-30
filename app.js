7fa3517b061283cf315d0ac25c71ad50895456e5
FZ2161 <filipzalewski44@gmail.com>
2024-04-30 14:44:59 +0200

    coś juz jest

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
-let roughSvg = rough.svg(document.getElementById('svg'));
-let kwadrat = roughSvg.rectangle(10,10,100,100);
+var roughSVG = rough.svg(document.getElementById('svg'));
+
+var globalX=300
+var globalY=300
+
+
+function drawSquareDown(){
+    const square = roughSVG.rectangle(globalX,globalY,100,100,{
+        fill: 'rgba(255,0,200,0.6)',
+        fillStyle: 'zigzag-line'    
+    })
+    svg.appendChild(square)
+
+    const line = roughSVG.line(globalX+50,globalY+100,globalX+50,globalY+120)
+    svg.appendChild(line)
+    globalY=globalY+120
+}
+
+const addSquare = document.getElementById("addSquareDown")
+addSquare.addEventListener("click", ()=>{
+    drawSquareDown()
+})
